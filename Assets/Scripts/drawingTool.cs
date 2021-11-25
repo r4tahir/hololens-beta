5e739ab60afce64b184ce1ee96da4cc4f5cd7a1a
r4tahir <r4tahir@edu.uwaterloo.ca>
2021-11-25 13:47:52 -0600

    nf com

--- Assets/Scripts/drawingTool.cs
+++ Assets/Scripts/drawingTool.cs
@@ -15,7 +15,7 @@ public class drawingTool : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerPos.Clear();
     }
 
     // Update is called once per frame
