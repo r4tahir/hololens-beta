cab6097e3eabd14150acfd00c560d0c4c5e6fceb
tomwym <tomwym@live.ca>
2021-11-30 16:12:45 -0500

    changed master structs, added button functionality

---
+++ Assets/Scripts/EventObjectScript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Vuforia;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventObjectScript : MonoBehaviour
+{
+    public List<Vector3> vecList;
+    private int index;
+    public List<string> ModelTargetNameList;
+    public List<GameObject> ModelTargetList;
+    public GameObject tmp;
+
+    void Start()
+    {
+        tmp = GameObject.Find("Button-ChangeCurrent");
+
+        index = 1;
+        ModelTargetNameList = new List<string> { "pointerMT", "boneMT" };
+        foreach (string s in ModelTargetNameList)
+        {
+            ModelTargetList.Add(GameObject.Find(s));
+        }
+        //ModelTargetList.Add(GameObject.Find("pointerMT"));
+        foreach (GameObject obj in ModelTargetList)
+        {
+            Debug.Log(obj.name);
+        }
+    }
+
+    public void ChangeActiveModelTarget()
+    {
+        ModelTargetList[index-1].SetActive(false);
+        index = index % ModelTargetList.Count +1;
+        ModelTargetList[index-1].SetActive(true);
+
+        Debug.Log("i am sad");
+        Debug.Log(ModelTargetList[index - 1].name);
+        // Text textfield = tmp.GetComponent<Text>();
+        // textfield.text = ModelTargetNameList[index - 1];
+        
+    }
+
+    public void addPointToVectorList()
+    {
+        GameObject ModelTarget = ModelTargetList[index - 1];
+
+        Status objStatus = ModelTarget.GetComponent<ObserverBehaviour>().TargetStatus.Status;
+        //Text textfield = GameObject.Find("/DebugCanvas").GetComponent<Text>();
+
+        if (objStatus == Status.NO_POSE)
+        {
+            Debug.Log("no pose try again");
+            //textfield.text = "no pose try again";
+        }
+        else
+        {
+            Debug.Log("pose found");
+            //textfield.text = "pose found";
+
+            Vector3 pos = ModelTarget.transform.localPosition;
+
+            GameObject ARCam = GameObject.Find("/ARCamera");
+            Vector3 ARCPos = ARCam.transform.localPosition;
+            Vector3 diff = pos - ARCPos;
+
+            List<Vector3> vecList = Master.vecList;
+            vecList.Add(diff);
+        }
+    }
+}
